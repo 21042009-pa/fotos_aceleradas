304ea37ed30f710d1d4c196cd852c2a9d7e5bd70
21042009-pa <paula.gianotto@aluno.senai.br>
2026-08-13 14:16:01 -0300

    concertei o import

--- fotos_aceleradas.js
+++ fotos_aceleradas.js
@@ -4,7 +4,6 @@ import { Accelerometer } from "expo-sensors";
 import { useEffect, useRef, useState } from "react";
 import { Button, Text, StyleSheet, View } from "react-native";
 import React from "react";
-import { useState } from "react";
 
 export default function CameraScreen() {
   // =========================
